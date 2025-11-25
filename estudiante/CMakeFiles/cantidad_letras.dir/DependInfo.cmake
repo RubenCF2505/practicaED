04eb5b14540282a1548950dcf2996dfcde9d8915
IntenseKyi <141171716+IntenseKyi@users.noreply.github.com>
2025-11-25 12:03:46 +0100

    Subida proyecto
    Casi solo el proyecto base directamente descargado de la práctica, tiene solo ciertas implementaciones del diccionario puesto, el resto está virgen.

---
+++ estudiante/CMakeFiles/cantidad_letras.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/inten/Desktop/Uni/Asignaturas/Anio-Redemption/ED/practicas/PF/estudiante/src/cantidad_letras.cpp" "CMakeFiles/cantidad_letras.dir/src/cantidad_letras.cpp.o" "gcc" "CMakeFiles/cantidad_letras.dir/src/cantidad_letras.cpp.o.d"
+  "/mnt/c/Users/inten/Desktop/Uni/Asignaturas/Anio-Redemption/ED/practicas/PF/estudiante/src/dictionary.cpp" "CMakeFiles/cantidad_letras.dir/src/dictionary.cpp.o" "gcc" "CMakeFiles/cantidad_letras.dir/src/dictionary.cpp.o.d"
+  "/mnt/c/Users/inten/Desktop/Uni/Asignaturas/Anio-Redemption/ED/practicas/PF/estudiante/src/letters_set.cpp" "CMakeFiles/cantidad_letras.dir/src/letters_set.cpp.o" "gcc" "CMakeFiles/cantidad_letras.dir/src/letters_set.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ estudiante/src/dictionary.cpp
@@ -0,0 +1,85 @@
+#include "dictionary.h"
+
+Dictionary::Dictionary() {}
+
+void Dictionary::clear()
+{
+
+}
+
+unsigned int Dictionary::size() const
+{
+
+}
+
+bool Dictionary::exists(const string &val)
+{
+
+}
+
+bool Dictionary::erase(const string &val)
+{
+
+}
+
+istream &operator>>(istream &is, Dictionary &dic)
+{
+
+}
+
+ostream &operator<<(ostream &os, const Dictionary &dic)
+{
+
+}
+
+int Dictionary::getOccurrences(const char c) const
+{
+
+}
+
+int Dictionary::getTotalLetters() const
+{
+
+}
+
+// ********** Implementación Iterador *****************************************
+
+Dictionary::iterator Dictionary::find(const string &w)
+{
+
+}
+
+vector<string> Dictionary::getWordsLength(int longitud)
+{
+
+}
+
+pair<Dictionary::iterator, bool> Dictionary::insert(const string &val)
+{
+
+}
+
+pair<Dictionary::iterator, Dictionary::iterator> Dictionary::range_prefix(const string &val)
+{
+
+}
+
+Dictionary::iterator Dictionary::begin() 
+{
+
+}
+
+Dictionary::const_iterator Dictionary::begin() const
+{
+
+}
+
+Dictionary::iterator Dictionary::end()
+{
+
+}
+
+Dictionary::const_iterator Dictionary::end() const
+{
+    
+}
